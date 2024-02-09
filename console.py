c36723c81e672c58849e27ffe19881ce3f07ee2e
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-02-08 23:24:59 -0800

    update is done

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """This module defines the entry point of the command interpreter."""
 import cmd
 import json
+import re
 from models import storage
 from models.base_model import BaseModel
 
@@ -36,8 +37,7 @@ class HBNBCommand(cmd.Cmd):
         args = arg.split()
         if not args:
             print("** class name missing **")
-
-        if args[0] not in my_classes.keys():
+        elif args[0] not in my_classes.keys():
             print("** class doesn't exist **")
         else:
             obj_new = my_classes[args[0]]()
@@ -51,40 +51,39 @@ class HBNBCommand(cmd.Cmd):
         args = arg.split()
         if not args:
             print("** class name missing **")
-
-        if args[0] not in my_classes.keys():
+        elif args[0] not in my_classes.keys():
             print("** class doesn't exist **")
-        if len(args) < 2:
+        elif len(args) < 2:
             print("** instance id missing **")
         else:
-            instance_id = args[1]
-            instance = args[0](instance_id)
-            if instance:
-                print(instance)
-            else:
+            instanceObjs = storage.all()
+            key = "{}.{}".format(args[0], args[1])
+            _instance = instanceObjs.get(key, None)
+            if _instance is None:
                 print("** no instance found **")
+                return
+            print(_instance )
 
     def do_destroy(self, arg):
         """Deletes an instance based on the class name and id"""
         args = arg.split()
         if not args:
             print("** class name missing **")
-            return
-        if args[0] not in my_classes.keys():
+        elif args[0] not in my_classes.keys():
             print("** class doesn't exist **")
-            return
-        if len(args) < 2:
+        elif len(args) < 2:
             print("** instance id missing **")
 
-        instanceObjs = storage.all()
-        key = "{}.{}".format(args[0], args[1])
-        _instance = instanceObjs.get(key, None)
-        if _instance is None:
-            print("** no instance found **")
-            return
+        else:
+            instanceObjs = storage.all()
+            key = "{}.{}".format(args[0], args[1])
+            _instance = instanceObjs.get(key, None)
+            if _instance is None:
+                print("** no instance found **")
+                return
 
-        del instanceObjs[key]
-        storage.save()
+            del instanceObjs[key]
+            storage.save()
 
     def do_all(self, arg):
         """Prints string representation of all instances.
@@ -101,7 +100,79 @@ class HBNBCommand(cmd.Cmd):
                   for _, v in _all.items() if type(v).__name__ == args[0]])
             return
 
-            
+    def do_update(self, arg):
+        """adding or updating attribute
+        update <class name> <id> <attribute name> "<attribute value>
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in my_classes.keys():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+            return
+        instanceObjs = storage.all()
+        key = "{}.{}".format(args[0], args[1])
+        _instance = instanceObjs.get(key, None)
+        if _instance is None:
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+        elif len(args) < 4:
+            print("** value missing **")
+        else:
+            match = re.findall(r"{.*}", arg)
+            if match:
+                _load = None
+                try:
+                    _load: dict = json.loads(match[0])
+                except Exception:
+                    print("** invalid syntax")
+                    return
+                for key, val in _load.items():
+                    setattr(_instance, key, val)
+                storage.save()
+                return
+            _attr = re.findall(r"^[\"\'](.*?)[\"\']", args[3])
+            if _attr:
+                setattr(_instance, args[2], _attr[0])
+            else:
+                valueList = args[3].split()
+                setattr(_instance, args[2], simple_str(valueList[0]))
+            storage.save()
+
+    def simple_str(arg):
+        """Parse `arg` to an `int`, `float` or `string`."""
+        par = re.sub("\"", "", arg)
+
+        if isInt(par):
+            return int(par)
+        elif isFloat(par):
+            return float(par)
+        else:
+            return arg
+
+    def isFloat(i):
+        """Checks if 'x'is float."""
+        try:
+            a = float(i)
+        except (TypeError, ValueError):
+            return False
+        else:
+            return True
+
+
+    def isInt(i):
+        """Checks if `x` is int."""
+        try:
+            a = float(i)
+            b = int(a)
+        except (TypeError, ValueError):
+            return False
+        else:
+            return a == b
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
