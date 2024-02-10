a9a3e5f9ebccc316fe3f6f3b0385ca978d453a10
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-02-10 03:50:36 -0800

    base

--- models/base_model.py
+++ models/base_model.py
@@ -2,49 +2,61 @@
 """
 Custom base class for the entire project
 """
-
+import models
 from uuid import uuid4
 from datetime import datetime
-import models
 
 
 class BaseModel:
-    """That defines all common attributes/methods for other classes"""
-    def __init__(self, *args, **kwargs):
-        """re-create an instance with this dictionary representation."""
-        if kwargs:
-            del kwargs["__class__"]
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    dtime_obj = datetime.fromisoformat(value)
-                    setattr(self, key, dtime_obj)
-                else:
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+    """
+    That defines:
+        all common attributes/methods for other classes
+    """
 
-            models.storage.new(self)
+    def __init__(self, *args, **kwargs):
+        """
+        re-create an instance:
+            with this dictionary representation.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+            return
+
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        models.storage.new(self)
 
     def save(self):
-        """pdates the public instance attribute updated_at"""
+        """
+        pdates the public instance attribute
+        with the current datetime updated_at
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def zh_to_dict(self):
-        """ returns a dictionary containing all keys/values of __dict__ """
-        dictReturn = {}
+        """
+        returns a dictionary containing all keys/values
+        of __dict__
+        """
+        dictReturn = {**self.__dict__}
+        dictReturn["__class__"] = type(self).__name__
+        dictReturn['created_at'] = dictReturn['created_at'].isoformat()
+        dictReturn['updated_at'] = dictReturn['updated_at'].isoformat()
 
-        dictReturn["__class__"] = self.__class__.__name__
-
-        for key, val in self.__dict__.items():
-            if isinstance(val, datetime):
-                dictReturn[key] = val.isoformat()
-            else:
-                dictReturn[key] = val
         return(dictReturn)
 
     def __str__(self):
-        """That return [<class name>] (<self.id>) <self.__dict__>"""
-        return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
+        """
+        That return:
+            [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
