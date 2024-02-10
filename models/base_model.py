34dc20815a823e79a7a4258b8b795d5d557e552a
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-02-10 09:32:21 -0800

    test base

--- models/base_model.py
+++ models/base_model.py
@@ -13,19 +13,24 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """re-create an instance with this dictionary representation."""
         if kwargs:
-            del kwargs["__class__"]
             for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
                 if key == "created_at" or key == "updated_at":
-                    dtime_obj = datetime.fromisoformat(value)
-                    setattr(self, key, dtime_obj)
-                else:
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
 
-            models.storage.new(self)
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        models.storage.new(self)
+
+    def __str__(self):
+        """That return [<class name>] (<self.id>) <self.__dict__>"""
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """pdates the public instance attribute updated_at"""
@@ -33,19 +38,10 @@ class BaseModel:
         models.storage.save()
 
     def zh_to_dict(self):
-        """ returns a dictionary containing all keys/values of __dict__ """
-        dictReturn = {}
+        """ returns a dictionary containing all keys/values of __dict__"""
+        dict = {**self.__dict__}
+        dict['__class__'] = type(self).__name__
+        dict['created_at'] = dict['created_at'].isoformat()
+        dict['updated_at'] = dict['updated_at'].isoformat()
 
-        dictReturn["__class__"] = self.__class__.__name__
-
-        for key, val in self.__dict__.items():
-            if isinstance(val, datetime):
-                dictReturn[key] = val.isoformat()
-            else:
-                dictReturn[key] = val
-        return(dictReturn)
-
-    def __str__(self):
-        """That return [<class name>] (<self.id>) <self.__dict__>"""
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
+        return dict
