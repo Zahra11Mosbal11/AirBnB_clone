0556e6ebe795df87fbe8b5e72f438b9b7c60c1cd
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-02-11 10:24:17 -0800

    test done

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -8,6 +8,9 @@ from datetime import datetime
 from models.amenity import Amenity
 from models.engine.file_storage import FileStorage
 
+amen1 = Amenity()
+amen2 = Amenity(**amen1.to_dict())
+
 
 class TestAmenity(unittest.TestCase):
     """Test cases for the `Amenity` class."""
@@ -21,50 +24,38 @@ class TestAmenity(unittest.TestCase):
         if os.path.exists(FileStorage._FileStorage__file_path):
             os.remove(FileStorage._FileStorage__file_path)
 
-    def test_params(self):
+    def test_attributes(self):
         """Test method for class attributes"""
 
-        a1 = Amenity()
-        a2 = Amenity(**a1.to_dict())
-        a3 = Amenity("hello", "wait", "in")
-
-        k = f"{type(a1).__name__}.{a1.id}"
-        self.assertIsInstance(a1.name, str)
-        self.assertIn(k, storage.all())
-        self.assertEqual(a3.name, "")
+        key = f"{type(amen1).__name__}.{amen1.id}"
+        self.assertIsInstance(amen1.name, str)
 
     def test_init(self):
         """Test method for public instances"""
-        a1 = Amenity()
-        a2 = Amenity(**a1.to_dict())
-        self.assertIsInstance(a1.id, str)
-        self.assertIsInstance(a1.created_at, datetime)
-        self.assertIsInstance(a1.updated_at, datetime)
-        self.assertEqual(a1.updated_at, a2.updated_at)
+        self.assertIsInstance(amen1.id, str)
+        self.assertIsInstance(amen1.created_at, datetime)
+        self.assertIsInstance(amen1.updated_at, datetime)
+        self.assertEqual(amen1.updated_at, amen2.updated_at)
 
     def test_str(self):
         """Test method for str representation"""
-        a1 = Amenity()
-        string = f"[{type(a1).__name__}] ({a1.id}) {a1.__dict__}"
-        self.assertEqual(a1.__str__(), string)
+        _str = f"[{type(amen1).__name__}] ({amen1.id}) {amen1.__dict__}"
+        self.assertEqual(amen1.__str__(), _str)
 
     def test_save(self):
         """Test method for save"""
-        a1 = Amenity()
-        old_update = a1.updated_at
-        a1.save()
-        self.assertNotEqual(a1.updated_at, old_update)
+        old_update = amen1.updated_at
+        amen1.save()
+        self.assertNotEqual(amen1.updated_at, old_update)
 
     def test_todict(self):
         """Test method for dict"""
-        a1 = Amenity()
-        a2 = Amenity(**a1.to_dict())
-        a_dict = a2.to_dict()
+        a_dict = amen2.to_dict()
         self.assertIsInstance(a_dict, dict)
-        self.assertEqual(a_dict['__class__'], type(a2).__name__)
+        self.assertEqual(a_dict['__class__'], type(amen2).__name__)
         self.assertIn('created_at', a_dict.keys())
         self.assertIn('updated_at', a_dict.keys())
-        self.assertNotEqual(a1, a2)
+        self.assertNotEqual(amen1, amen2)
 
 
 if __name__ == "__main__":
